dc1305eb75f9d2a118f5144f5e48093315f555b6
ArjunKini <39121415+ArjunKini@users.noreply.github.com>
2019-09-03 17:52:15 +0530

    Update main.py
    Removed mind download and getParagraphFeatures import

--- GPT-Deployment/mind-serving-lambda-gpt/main.py
+++ GPT-Deployment/mind-serving-lambda-gpt/main.py
@@ -15,7 +15,7 @@ import requests
 
 import torch
 from gpt import OpenAIGPTPreTrainedModel,OpenAIGPTConfig,OpenAIGPTModel,SequenceSummary
-from mind_utils import getSentenceFeatures, getParagraphFeatures, CustomOpenAIGPTDoubleHeadsModel, NumpyEncoder
+from gpt.mind_utils import getSentenceFeatures, CustomOpenAIGPTDoubleHeadsModel, NumpyEncoder
 import numpy as np
 import pickle
 
@@ -25,23 +25,20 @@ logger.setLevel(logging.INFO)
 
 def load_model_files():
 
-    bucket = os.getenv('BUCKET_NAME', 'io.etherlabs.gpt.artifacts')
+    bucket = os.getenv('BUCKET_NAME', 'io.etherlabs.artifacts')
     model_path = os.getenv('MODEL')
-    mind_path = os.getenv('MIND')
+    # CONFIG = staging2/minds/[mindid]/gpt_config.json
     config_path = os.getenv('CONFIG')
     modelObj = s3.Object(
         bucket_name=bucket,
         key=model_path
     )
-    mind_dl_path = os.path.join(os.sep, 'tmp', 'mind.pkl')
-    s3.Bucket(bucket).download_file(mind_path,mind_dl_path)
 
     config_dl_path = os.path.join(os.sep, 'tmp', 'gpt_config.json')
     s3.Bucket(bucket).download_file(config_path,config_dl_path)
 
     state_dict = torch.load(io.BytesIO(modelObj.get()["Body"].read()),map_location='cpu')
-    mind_dict = pickle.load(open(mind_dl_path,'rb'))
-    return state_dict,mind_dict,config_dl_path
+    return state_dict,config_dl_path
 
 def process_input(json_request):
     if isinstance(json_request, str):
@@ -50,7 +47,7 @@ def process_input(json_request):
     return text_request
 
 # load the model when lambda execution context is created
-state_dict,mind_dict, config_path = load_model_files()
+state_dict,config_path = load_model_files()
 config = OpenAIGPTConfig(config_path)
 config.vocab_size = config.vocab_size + config.n_special
 model = CustomOpenAIGPTDoubleHeadsModel(config)
@@ -59,8 +56,8 @@ model.eval()
 
 def lambda_handler(event, context):
     logger.info(event)
-    input_text = process_input(event['body'])
-    model_feats = getSentenceFeatures(model, mind_dict, input_text)
+    input_list = process_input(event['body'])
+    model_feats = getSentenceFeatures(model, input_list)
     response = json.dumps(model_feats,cls=NumpyEncoder)
 
     return {
