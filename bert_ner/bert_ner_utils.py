6af1f19a44b674867f191c26b1c066a7d190af00
ArjunKini <39121415+ArjunKini@users.noreply.github.com>
2019-10-29 03:17:41 +0530

    Update bert_ner_utils.py
    Refactored get_entities function.

--- bert_ner/bert_ner_utils.py
+++ bert_ner/bert_ner_utils.py
@@ -37,83 +37,41 @@ class BERT_NER():
         self.sm = torch.nn.Softmax(dim=1)
         self.labels = labels
 
-    def get_entities(self, text, get_non_entities=False):
-        # input: text - str
-        # output: entity tokens - list(str), confidence scores - list(float)
-        scores=[]
-        labels=[]
+    
+    def get_entities(self,text,get_non_entities=False):
+        input_ids=[]
         tokenized_text = self.tokenize(text)
-        encoded_text = self.tokenizer.encode(text)
-        
+        token_to_word=[]
+        i=0
+        for word in text.split():
+            toks = self.tokenizer.encode(word)
+            token_to_word.extend([word.lower().strip(".,?!'\"")]*len(toks))
+            input_ids.extend(toks)
         # Calculating batch size based on nearest "." from mid-point of text if length exceeds 512
-        if len(encoded_text)>512:
-            mid = len(encoded_text)//2
-            batch_size = mid - 1 - encoded_text[:mid][::-1].index(self.tokenizer.encode(".")[0])
+        if len(input_ids)>512:
+            mid = len(input_ids)//2
+            batch_size = mid - 1 - input_ids[:mid][::-1].index(self.tokenizer.encode(".")[0])
         else:
             batch_size = 510 
         
-        for i in range(0,len(encoded_text),batch_size):
-            encoded_text_sp = self.tokenizer.encode("[CLS]") + encoded_text[i:i+batch_size] + self.tokenizer.encode("[SEP]")
+        for i in range(0,len(input_ids),batch_size):
+            encoded_text_sp = self.tokenizer.encode("[CLS]") + input_ids[i:i+batch_size] + self.tokenizer.encode("[SEP]")
             input_ids = torch.tensor(encoded_text_sp).unsqueeze(0)
-            outputs = self.model(input_ids)[0][0,1:-1]
-            
-            scores.extend(list(self.sm(outputs).max(-1)[0].detach().numpy()))
-            labels.extend(list([self.labels[y] for y in self.sm(outputs).argmax(-1).detach().numpy()]))
-        
-        entities=[]
-        non_entities = []
-        non_conf=[]
-        conf = []
-        seen=[]
-        
-        for ind,(tok_text,label) in enumerate(zip(tokenized_text,labels)):
-            if label=="O" and ind not in seen:
-                # Check for false negative (low confidence of "O" label)
-                confidence_thresh = 0.95
-                size_thresh=1
-                if scores[ind] < confidence_thresh and len(tok_text)>size_thresh:
-                    label="E"
-                    scores[ind] = 1-scores[ind]
+            entities={}
+            non_entities={}
+            with torch.no_grad():
+                outputs = self.model(input_ids)[0][0,1:-1]
+            for i,(tok,embed) in enumerate(zip(token_to_word,list(outputs))):
+                embed=embed.unsqueeze(0)
+                score = self.sm(embed).detach().numpy().max(-1)[0]
+                label = self.labels[self.sm(embed).argmax().detach().numpy()] 
+                if label!="O" or (label=="O" and score<0.95):
+                    entities[token_to_word[i]] = max(entities.get(token_to_word[i],0),score)
                 else:
-                    non_entities.append(tok_text)
-                    non_conf.append(scores[ind])
-                
-            if label!="O" and ind not in seen:
-                # If current token is mid-token(starts with #), go back till you find source token
-                if '##' in tok_text: 
-                    k=1
-                    tmp=[]
-                    insert_pos = len(entities) if len(entities)>=1 else 0
-                    while(tokenized_text[ind-k][0]=="#"):
-                        conf.insert(insert_pos,scores[ind-k])
-                        entities.insert(insert_pos,tokenized_text[ind-k])
-                        seen.append(ind-k)
-                        k+=1
-                    
-                    seen.append(ind-k)
-                    conf.insert(insert_pos,scores[ind-k]) 
-                    entities.insert(insert_pos,tokenized_text[ind-k])
-
-                # Append current token
-                entities.append(tok_text)
-                conf.append(scores[ind])
-                seen.append(ind)
-
-                # Check for additional tokens and append
-                if ind < len(tokenized_text)-1:
-                    if tokenized_text[ind+1][0]=="#":
-                        k=1
-                        while ind+k <len(tokenized_text) and (tokenized_text[ind+k][0]=="#"):
-                            entities.append(tokenized_text[ind+k])
-                            seen.append(ind+k)
-                            conf.append(scores[ind+k])
-                            k+=1
-
+                    non_entities[tokenized_text[i]] = score
         if get_non_entities:
-            return entities, conf, non_entities, non_conf
-        else:
-            return entities, conf
-
+            return list(entities.keys()),list(entities.values()),list(non_entities.keys()),list(non_entities.values())
+        return list(entities.keys()),list(entities.values())
     def tokenize(self,text):
         return self.tokenizer.tokenize(text)
 
@@ -122,4 +80,4 @@ class BERT_NER():
         if capitalize:
             return list(map(lambda x: x.capitalize(),words.split()))
         else:
-            return list(map(lambda x: x.upper(),words.split()))
+            return words.upper().split()
