a4f39120b0d6393ffc9cf30f4973c525a7239a30
ArjunKini <39121415+ArjunKini@users.noreply.github.com>
2019-11-13 10:19:49 +0530

    Update bert_ner_utils_deployment
    Corrected removal of consecutive duplicate entities logic, added check for empty token.

--- bert_ner/bert_ner_utils_deployment.py
+++ bert_ner/bert_ner_utils_deployment.py
@@ -17,6 +17,7 @@ import torch
 import torch.nn as nn
 import re
 from nltk import sent_tokenize
+from itertools import groupby 
 
 class BertForTokenClassification_custom(BertPreTrainedModel):
     def __init__(self, 
@@ -69,6 +70,7 @@ class BERT_NER():
             tok_path=model_path
         self.tokenizer = BertTokenizer(tok_path+"vocab.txt")
         self.sm = nn.Softmax(dim=1)
+        self.conf = 0.995
         self.contractions = {"[sep]":"separator","[cls]":"classify","ain't": 'am not', "aren't": 'are not', "can't": 'cannot', "can't've": 'cannot have', "'cause": 'because', "could've": 'could have', "couldn't": 'could not', "couldn't've": 'could not have', "didn't": 'did not', "doesn't": 'does not', "don't": 'do not', "hadn't": 'had not', "hadn't've": 'had not have', "hasn't": 'has not', "haven't": 'have not', "he'd": 'he would', "he'd've": 'he would have', "he'll": 'he will', "he's": 'he is', "how'd": 'how did', "how'll": 'how will', "how's": 'how is', "i'd": 'i would', "i'll": 'i will', "i'm": 'i am', "i've": 'i have', "isn't": 'is not', "it'd": 'it would', "it'll": 'it will', "it's": 'it is', "let's": 'let us', "ma'am": 'madam', "mayn't": 'may not', "might've": 'might have', "mightn't": 'might not', "must've": 'must have', "mustn't": 'must not', "needn't": 'need not', "oughtn't": 'ought not', "shan't": 'shall not', "sha'n't": 'shall not', "she'd": 'she would', "she'll": 'she will', "she's": 'she is', "should've": 'should have', "shouldn't": 'should not', "that'd": 'that would', "that's": 'that is', "there'd": 'there had', "there's": 'there is', "they'd": 'they would', "they'll": 'they will', "they're": 'they are', "they've": 'they have', "wasn't": 'was not', "we'd": 'we would', "we'll": 'we will', "we're": 'we are', "we've": 'we have', "weren't": 'were not', "what'll": 'what will', "what're": 'what are', "what's": 'what is', "what've": 'what have', "where'd": 'where did', "where's": 'where is', "who'll": 'who will', "who's": 'who is', "won't": 'will not', "wouldn't": 'would not', "you'd": 'you would', "you'll": 'you will', "you're": 'you are'}
     
     def replace_contractions(self, 
@@ -93,14 +95,11 @@ class BERT_NER():
 
         # removing duplicate entities
         seg_entities = dict(zip(segment_entities,segment_scores))
-        seg_words = list(seg_entities.keys())
-        seg_scores = list(seg_entities.values())
-        return seg_words, seg_scores
+        return seg_entities
             
     def get_entities_from_sentence(self, 
                                    text):
         input_ids=[]
-        tokenized_text = self.tokenize(text)
         token_to_word=[]
         
         # cleaning and splitting text, preserving punctuation
@@ -141,8 +140,9 @@ class BERT_NER():
                 score = self.sm(embed).detach().numpy().max(-1)[0]
                 label = self.labels[self.sm(embed).argmax().detach().numpy()]
                 # Consider Entities and Non-Entities with low confidence (false negatives)
-                if label!="O" or (label=="O" and score<0.98):
-                    entities.append((tok,score))
+                if tok!='':
+                    if label!="O" or (label=="O" and score<self.conf):
+                        entities.append((tok,score))
         return entities
     
     def tokenize(self, 
@@ -156,10 +156,10 @@ class BERT_NER():
         sent_scores=[]
         seen=[]
         # handling abbreviations such as U.S.
-        text = re.sub("[A-Z][.]\s?[A-Z][.]?",lambda mobj: mobj.group(0)[0] + " " + mobj.group(0)[-2],text).casefold()
-        
+#         text = re.sub("[A-Z][.]\s?[A-Z][.]?",lambda mobj: mobj.group(0)[0] + " " + mobj.group(0)[-2],text).casefold()
+        text = re.sub("[A-Z][.]\s?",lambda mobj: mobj.group(0)[0]+" ",text).casefold()
         # remove consecutive duplicate entities
-        entity_words = list(dict.fromkeys([e[0] for e in entities if e[0]!='']))
+        entity_words = [i[0] for i in groupby(map(lambda x: x[0],entities))]
         entity_scores = {e[0]:e[1] for e in entities}
         for i in range(len(entity_words)):
             if i in seen:
@@ -185,3 +185,11 @@ class BERT_NER():
                 sent_scores += [score/(k-i)]
         
         return sent_entity_list,sent_scores
+
+
+
+
+
+
+
+
